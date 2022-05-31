c567022ae4519daf865cfe99b678f07150de488f
ksyng75 <ksyng75@naver.com>
2022-05-31 15:46:04 +0900

    05031-fifth

--- src/main/java/me/ng/mapper/BoardMapper.java
+++ src/main/java/me/ng/mapper/BoardMapper.java
@@ -8,4 +8,12 @@ import me.ng.model.Board;
 public interface BoardMapper {
 
 	List<Board> getList();
+	
+	Board findByBno(Long board);
+	
+	void insert(Board board);
+	
+	void delete(Board board);
+	
+	void update(Long bno);
 }
